101a00bda8b4016efcdbe97f33bf095a4fd029e3
Gabriel Fray Garandy <gabrielfraygarandy@gmail.com>
2022-07-05 18:32:26 -0300

    feat: Adicionei o metodo estático Delete

--- src/js/Api.js
+++ src/js/Api.js
@@ -1,29 +1,33 @@
 class Api {
+  static async listarClientes() {
+    const response = await fetch(
+      "https://atividade-api-clientes.herokuapp.com/clientes"
+    );
+    const data = await response.json();
 
-    static async listarClientes(){
+    return data;
+  }
 
-        const response = await fetch("https://atividade-api-clientes.herokuapp.com/clientes")
-        const data     = await response.json()
+  static async editarCliente(id, data) {
+    await fetch("https://atividade-api-clientes.herokuapp.com/clientes/" + id, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+  }
 
-        return data
+  static async cadastrarCliente(data) {}
 
-    }
-    
-    
-    static async editarCliente(id, data){
-
-    }
-    
-
-    static async cadastrarCliente(data){
-
-    }
-
-
-    static async deletarCliente(id){
-
-    }
-
+  static async deletarCliente(id) {
+    const response = await fetch(
+      "https://atividade-api-clientes.herokuapp.com/clientes/" + id,
+      {
+        method: "DELETE",
+      }
+    );
+  }
 }
 
-export {Api}
+export default Api;
